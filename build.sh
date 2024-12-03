01b49c6310f66742b58b981cc542f2cb11fdba00
bobo <bobodai515@163.com>
2024-12-03 15:27:00 +0800

    feat: exercise 3

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # 设置目标路径
 TARGET_DIR="../arceos/payload"
-APP_NAME="hello_app"
-APP2_NAME="ebreak"
-APP_BIN="$APP_NAME.bin"
+APP2_NAME="hello_app"
+APP1_NAME="nop"
 APP2_BIN="$APP2_NAME.bin"
+APP1_BIN="$APP1_NAME.bin"
 APPS_BIN="apps.bin"
 HEADER_BIN="header.bin"
 
@@ -15,15 +15,15 @@ cargo build --target riscv64gc-unknown-none-elf --release
 
 # 转换为裸机二进制文件
 echo "Converting ELF to binary format..."
-rust-objcopy --binary-architecture=riscv64 --strip-all -O binary target/riscv64gc-unknown-none-elf/release/$APP_NAME ./$APP_BIN
+rust-objcopy --binary-architecture=riscv64 --strip-all -O binary target/riscv64gc-unknown-none-elf/release/$APP2_NAME ./$APP2_BIN
 
 # 构建第二个项目
-riscv64-unknown-elf-as -o ebreak.o ebreak.s && riscv64-unknown-elf-ld -o ebreak.elf ebreak.o && riscv64-unknown-elf-objcopy -O binary ebreak.elf ebreak.bin
+riscv64-unknown-elf-as -o nop.o nop.s && riscv64-unknown-elf-ld -o nop.elf nop.o && riscv64-unknown-elf-objcopy -O binary nop.elf nop.bin
 
 # 计算程序大小
-APP_SIZE=$(stat -f %z ./$APP_BIN) # for mac
 APP2_SIZE=$(stat -f %z ./$APP2_BIN) # for mac
-#APP_SIZE=$(stat --format="%s" ./$APP_BIN) # for linux
+APP1_SIZE=$(stat -f %z ./$APP1_BIN) # for mac
+#APP2_SIZE=$(stat --format="%s" ./$APP2_BIN) # for linux
 
 APP_NUM=2
 
@@ -34,10 +34,10 @@ printf "%02x" "$(( APP_NUM & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
 
 # 创建头部文件（8字节：4字节应用程序大小 + 4字节应用程序大小）
 echo "Add header with application size..."
-printf "%02x" "$(( (APP_SIZE >> 24) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
-printf "%02x" "$(( (APP_SIZE >> 16) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
-printf "%02x" "$(( (APP_SIZE >> 8) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
-printf "%02x" "$((APP_SIZE & 0xFF))" | xxd -r -p >> "$HEADER_BIN"
+printf "%02x" "$(( (APP1_SIZE >> 24) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
+printf "%02x" "$(( (APP1_SIZE >> 16) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
+printf "%02x" "$(( (APP1_SIZE >> 8) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
+printf "%02x" "$((APP1_SIZE & 0xFF))" | xxd -r -p >> "$HEADER_BIN"
 
 printf "%02x" "$(( (APP2_SIZE >> 24) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
 printf "%02x" "$(( (APP2_SIZE >> 16) & 0xFF ))" | xxd -r -p >> "$HEADER_BIN"
@@ -57,12 +57,12 @@ echo "Writing header into $APPS_BIN..."
 dd if=$HEADER_BIN of=./$APPS_BIN bs=1 seek=0 conv=notrunc
 
 # 将 hello_app.bin 写入 apps.bin
-echo "Writing $APP_BIN into $APPS_BIN..."
-dd if=./$APP_BIN of=./$APPS_BIN bs=1 seek=$APP_HEADER_SIZE conv=notrunc
+echo "Writing $APP2_BIN into $APPS_BIN..."
+dd if=./$APP1_BIN of=./$APPS_BIN bs=1 seek=$APP_HEADER_SIZE conv=notrunc
 
 # 将 hello_app.bin 写入 apps.bin
-echo "Writing $APP_BIN into $APPS_BIN..."
-dd if=./$APP2_BIN of=./$APPS_BIN bs=1 seek=$((APP_HEADER_SIZE + APP_SIZE)) conv=notrunc
+echo "Writing $APP2_BIN into $APPS_BIN..."
+dd if=./$APP2_BIN of=./$APPS_BIN bs=1 seek=$((APP_HEADER_SIZE + APP1_SIZE)) conv=notrunc
 
 # 确保目标目录存在
 echo "Ensuring target directory exists..."
