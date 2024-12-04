529cd4d2642357f511ace35a004351efea10597f
bobo <bobodai515@163.com>
2024-12-04 18:50:02 +0800

    feat: exercise 5

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,51 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    core::arch::asm!(
-    "wfi",
-    options(noreturn)
+    hello();
+    putchar('A');
+    shutdown();
+}
+
+unsafe fn hello() {
+    core::arch::asm!("
+    li      t0, {abi_num}
+    slli    t0, t0, 3
+    add     t1, a7, t0
+    ld      t1, (t1)
+    jalr    t1",
+    abi_num = const SYS_HELLO,
+    )
+}
+
+unsafe fn putchar(c: char) {
+    let arg0: u8 = c as u8;
+    core::arch::asm!("
+    li      t0, {abi_num}
+    slli    t0, t0, 3
+    add     t1, a7, t0
+    ld      t1, (t1)
+    jalr    t1",
+    abi_num = const SYS_PUTCHAR,
+    in("a0") arg0,
+    )
+}
+
+unsafe fn shutdown() -> ! {
+    core::arch::asm!("
+    li      t0, {abi_num}
+    slli    t0, t0, 3
+    add     t1, a7, t0
+    ld      t1, (t1)
+    jalr    t1
+    j       .",
+    abi_num = const SYS_TERMINATE,
+    options(noreturn),
     )
 }
 
